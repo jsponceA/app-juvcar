05e8765eebd11c5bee758b2378632b0e597dc83d
unknown <andres07172@gmail.com>
2025-10-02 22:17:25 -0400

    Agregue un parrafo en el home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
       <Navbar />
       <h1>ESTA SERA LA PAGINA DE INICIO</h1>
       <img src="/logo_juan_5-removebg-preview.png" alt="" className="" />
+      <p className="text-blue-500 ">Bienvenido a nuestra página de inicio. Aquí encontrarás información sobre nuestros servicios y productos.</p>
       <Footer />
     </div>
   );
